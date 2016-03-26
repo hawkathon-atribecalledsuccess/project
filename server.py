1c07eabe6f278eb52b3818b6f3c02b2705956124
Thomas Hsu <tomdenotela@gmail.com>
2016-03-25 23:06:50 -0400

    Server test.

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, abort
+app = Flask(__name__)
+
+@app.route('/onText', methods='POST')
+def onText():
+	if not request.json:
+		return abort(500)
+	else:
+		print request.json
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host='0.0.0.0')
+
+

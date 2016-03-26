3530b608f6615e9b98259599ad0c780c3fa6e212
Thomas Hsu <tomdenotela@gmail.com>
2016-03-26 01:03:41 -0400

    Changed function names.

--- logic.py
+++ logic.py
@@ -8,13 +8,13 @@ class Logic:
 		sec = twilio['secret']
 		self.twilio = TwilioRestClient(sid, sec)
 
-	def open(self, phone_number, doorName):
+	def openDoor(self, phone_number, doorName):
 		# add door logic here. 
 		msg = 'Opened door %s. ' % (doorName)
 		self.twilio.messages.create(to=phone_number, from_="+16262437676", body=msg)
 		print msg
 
-	def update_log(self, log_entry):
+	def updateLog(self, log_entry):
 		self.log.append(log_entry)
 
 	def getLog(self):
